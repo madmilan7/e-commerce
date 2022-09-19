75c23d1a0069e886aad00664af16280bbc9fff2e
Mahdi Bagheri <qarjis8@gmail.com>
2022-09-19 21:38:02 +0430

    add itemsCounter to cart icon in Navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 // Icons
 import cart from '../../assets/icons/cart.svg';
@@ -7,23 +8,29 @@ import login from '../../assets/icons/login.svg';
 import search from '../../assets/icons/search.svg';
 
 const Navbar = () => {
+
+    const counter = useSelector(state => state.cartState.itemsCounter);
+
     return (
-            <div className='h-24 mx-24 flex justify-between items-center mt-0 p-3'>
-                <div className='flex items-center'>
-                    <Link to="/"><h1 className='text-3xl p-3 font-bold text-purple-800'>moboland</h1></Link>
-                    <img className='w-6 ml-4' src={search} alt="search" />
-                </div>
-                <ul className='flex gap-12 text-xl'>
-                    <li className='transition-all delay-100 cursor-pointer border-transparent border-b-4 border-solid hover:border-b-purple-800 p-1'>phone</li>
-                    <li className='transition-all delay-100 cursor-pointer border-transparent border-b-4 border-solid hover:border-b-purple-800 p-1'>phone</li>
-                    <li className='transition-all delay-100 cursor-pointer border-transparent border-b-4 border-solid hover:border-b-purple-800 p-1'>phone</li>
-                    <li className='transition-all delay-100 cursor-pointer border-transparent border-b-4 border-solid hover:border-b-purple-800 p-1'>phone</li>
-                </ul>
-                <div className='flex font-bold gap-6 mt-2 justify-center items-center'>
-                    <img className='relative w-8' src={cart} alt="cart" />
-                    <Link to="/signup"><img className='w-7' src={login} alt="login" /></Link>
-                </div>
+        <div className='h-24 mx-24 flex justify-between items-center mt-0 p-3'>
+            <div className='flex items-center'>
+                <Link to="/"><h1 className='text-3xl p-3 font-bold text-purple-800'>moboland</h1></Link>
+                <img className='w-6 ml-4' src={search} alt="search" />
             </div>
+            <ul className='flex gap-12 text-xl'>
+                <Link to="/products"><li className='transition-all delay-100 cursor-pointer border-transparent border-b-4 border-solid hover:border-b-purple-800 p-1'>Products</li></Link>
+                <li className='transition-all delay-100 cursor-pointer border-transparent border-b-4 border-solid hover:border-b-purple-800 p-1'>phone</li>
+                <li className='transition-all delay-100 cursor-pointer border-transparent border-b-4 border-solid hover:border-b-purple-800 p-1'>phone</li>
+                <li className='transition-all delay-100 cursor-pointer border-transparent border-b-4 border-solid hover:border-b-purple-800 p-1'>phone</li>
+            </ul>
+            <div className='flex font-bold gap-6 mt-2 justify-center items-center'>
+                <div>
+                    <Link to="/cart"><img className='relative w-8' src={cart} alt="cart" /></Link>
+                    <span>{counter}</span>
+                </div>
+                <Link to="/signup"><img className='w-7' src={login} alt="login" /></Link>
+            </div>
+        </div>
     );
 };
 
